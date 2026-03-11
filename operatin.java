9cd9775a943dfee15d0124cdd30fb2a4cf0721d8
manojnitte58-cyber <manojnitte58@gmail.com>
2026-03-11 10:03:15 +0530

    Create operation class to sum two numbers
    Fix syntax errors and implement sum calculation.

---
+++ operatin.java
@@ -0,0 +1,11 @@
+import util.java.Scanner
+class operation
+  {
+    public static void main(string arg[])
+    {
+       int a=10;
+      int b=20;
+      system.out.println('the sum of two numbers:',sum)
+        }
+  }
+   
